330ea87073bd5c0f12ffebdec7b75208ffe2ef5e
Mynor Morales <mynordma@gmail.com>
2025-05-28 19:48:27 -0600

    last commit

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,20 @@
 <?php
 $host = 'localhost';
-$db   = 'TM';
-$user = 'root';
-$pass = 'mynordma';
+$db   = 'insert_schema';
+$user = 'insert_user';
+$pass = 'insert_password';
 $charset = 'utf8mb4';
+
+/*
+    CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status ENUM('pending','done') DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+
+ */
 
 $options = [
   PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
